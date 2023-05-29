7bd677f0a1a4914379bf8372a9899180dc3d304a
julia leal <julialeal.t@gmail.com>
2023-05-29 19:38:29 -0300

    executavel do cucumber

---
+++ app/src/test/java/CucumberRunner.java
@@ -0,0 +1,13 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        tags = "not @wip and not @quarantine",
+        plugin = {"pretty", "html:build/reports/feature.html"},
+        features = {"src/test/resources/features"}
+)
+public class CucumberRunner {
+    
+}
